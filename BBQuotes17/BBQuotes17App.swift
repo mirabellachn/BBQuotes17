5ff0b5cf161086fdb983ed1e9d7f149d555287b4
mirabellachn <mirabellacn@gmail.com>
2024-12-11 17:29:06 +0700

    init

---
+++ BBQuotes17/BBQuotes17App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes17App.swift
+//  BBQuotes17
+//
+//  Created by mirabella  on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
